27e405567c392deafa41a914f16662999ff8ce59
Pojoke <pavlykdmutro@gmail.com>
2024-12-08 13:19:21 +0200

    dz

---
+++ hello.php
@@ -0,0 +1,22 @@
+
+
+<?php
+// Строка:
+$string = "Hello!";
+// Загрузка рисунка фона
+$im = imageCreateFromPNG("./images images.png");
+// Создание в палитре нового цвета — черного.
+$color = imageColorAllocate($im, 255, 0, 0);
+// Размер шрифта
+$size = 5;
+// Сдвиг надписи
+$offset = strlen($string) / 2 * $size;
+// Вычисление x для расположения текста по ширине
+$x = (imageSX($im) - strlen($string)) / 2 - $offset;
+$y = 125;
+// Вывод строки поверх загруженного изображения.
+imageString($im, $size, $x, $y, $string, $color);
+// Вывод картинки в стандартный выходной поток - в браузер
+header("Content-type: image/png");
+imagePng($im); // освобождение памяти, занятой картинкой
+imageDestroy($im);
