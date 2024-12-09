9f9d591c2fc00cd2ccfc3e77d7be8241c08c607c
Pojoke <pavlykdmutro@gmail.com>
2024-12-09 19:21:08 +0200

    dz

--- image.php
+++ image.php
@@ -1,13 +1,14 @@
 <?php
 // Создание изображения 100*30
-$im = imagecreate(100, 30);
+$im=imagecreatefromjpeg("./images/O2-Icon.jpg");
 // Желтый фон, синий текст
-$bg = imagecolorallocate($im, 255, 255, 0);
-$textcolor = imagecolorallocate($im, 0, 0, 255);
+// $bg = imagecolorallocate($im, 255, 255, 0);
+$textcolor = imagecolorallocatealpha($im, 0, 0, 0, 90);
+$transparen=imageColorTransparent($im, $textcolor);
 // Надпись в левом верхнем углу
-imagestring($im, 5, 0, 0, 'Hello world!', $textcolor);
+imagestring($im, 5, 50, 50, 'My_images', $textcolor);
 // Вывод изображения в стандартный выходной поток - в браузер
-header('Content-type: image/png');
+header('Content-type: image/jpg');
 imagepng($im);
 //освобождение памяти, занятой картинкой
 imagedestroy($im);
